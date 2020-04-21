e853115c5a11d3ea2208637b935a02c7c3329a2f
Gustavo\xpand <gustavo.oliveira@xpand-it.com>
2020-04-21 10:43:26 +0100

    Signup working

--- src/components/signup/SignupComponent.vue
+++ src/components/signup/SignupComponent.vue
@@ -3,13 +3,39 @@
     <v-row>
       <v-col>
         <h1>Signup</h1>
-        <v-form>
-          <v-text-field label="Email" type="email"></v-text-field>
-          <v-autocomplete label="Which browser do you use?" :items="browsers"></v-autocomplete>
-          <v-file-input label="Attach profile picture"></v-file-input>
-          <v-text-field label="Birthday" v-model="birthday" readonly></v-text-field>
+        <v-form @submit.prevent="onSubmit">
+          <v-text-field label="Email" type="email" v-model="email" id="email"></v-text-field>
+          <v-text-field
+            v-model="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Normal with hint text"
+            hint="At least 8 characters"
+            counter
+            @click:append="show1 = !show1"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="confirmPassword"
+            :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show2 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Normal with hint text"
+            hint="Should be the same as the Password"
+            counter
+            @click:append="show2 = !show2"
+          ></v-text-field>
+          <!--
+          <v-autocomplete
+            label="Which browser do you use?"
+            :items="browsersList"
+            v-model="browsers"
+          ></v-autocomplete>
+          <v-file-input label="Attach profile picture" v-model="picture"></v-file-input>
+          <v-text-field label="Birthday" readonly></v-text-field>
           <v-date-picker v-model="birthday"></v-date-picker>
-          <v-checkbox label="Agree to terms & conditions"></v-checkbox>
+          <v-checkbox label="Agree to terms & conditions"></v-checkbox>-->
           <v-btn type="submit" color="primary">Submit</v-btn>
         </v-form>
       </v-col>
@@ -18,11 +44,52 @@
 </template>
 
 <script>
+import axios from '../../utils/axios-auth'
 export default {
   data() {
     return {
+      show1: false,
+      show2: false,
+      /* rules: {
+        required: value => !!value || 'Required.',
+        min: v => v.length >= 8 || 'Min 8 characters'
+      },*/
+
+      //fields
+      email: '',
+      password: '',
+      confirmPassword: '',
+      browsers: '',
+      picture: '',
       birthday: '',
-      browsers: ['Chrome', 'Firefox', 'Safari', 'Edge']
+      browsersList: ['Chrome', 'Firefox', 'Safari', 'Edge']
+    }
+  },
+  methods: {
+    onSubmit() {
+      const formData = {
+        email: this.email,
+        //age: this.age,
+        password: this.password,
+        confirmPassword: this.confirmPassword
+        //country: this.country,
+        //hobbies: this.hobbyInputs.map(hobby => hobby.value),
+        //terms: this.terms
+      }
+      console.log(formData)
+      axios
+        .post('/accounts:signUp?key=AIzaSyBuNcXIsSB8G1oVeaixLHEQfzGlb54EKoA', {
+          email: formData.email,
+          password: formData.password,
+          returnSecureToken: true
+        })
+        .then(result => {
+          console.log(result)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      //this.$store.dispatch('signup', formData)
     }
   }
 }
---
+++ src/store/modules/authentication.js
@@ -0,0 +1,25 @@
+//import axios from './axios-auth'
+//import globalAxios from 'axios'
+
+const state = {
+  idToken: null,
+  userId: null,
+  user: null
+}
+
+const mutations = {}
+
+const actions = {}
+
+const getters = {
+  /*todos: state => {
+    return state.todos
+  }*/
+}
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
